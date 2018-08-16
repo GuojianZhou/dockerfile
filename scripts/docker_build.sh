fb5200e8cb7af2f0da54c14971612738cc818f0c
Joe <joe_zgj@163.com>
2018-08-16 19:19:31 +0800

    Add the CUDA_bazel Dockerfile
    Signed-off-by: Joe <joe_zgj@163.com>

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -23,9 +23,24 @@ case ${DOCKER_IMAGE} in
 		cp ${TOP_DIR}/images/ubuntu-base-16.04.4-base-amd64.tar.gz .
 		echo "The nv/cuda-9.2_cudnn-7.2:V0.1 docker image will be created!"
 		docker build -t nv/cuda-9.2_cudnn-7.2:V0.1 .
-		echo "The nv/cuda-9.2_cudnn-7.2:V0.1:V0.1 docker image has be created!"
+		echo "The nv/cuda-9.2_cudnn-7.2:V0.1 docker image has be created!"
 		if [ -f ubuntu-base-16.04.4-base-amd64.tar.gz ]; then
 			rm -f ubuntu-base-16.04.4-base-amd64.tar.gz
+		fi
+		popd
+		;;
+	"CUDA-bazel")
+		pushd ${TOP_DIR}/CUDA/CUDA-9.2.bazel.V0.2
+		cp ${TOP_DIR}/images/ubuntu-base-16.04.4-base-amd64.tar.gz .
+		cp ${TOP_DIR}/images/bazel-0.5.4-installer-linux-x86_64.sh .
+		echo "The nv/cuda-9.2_cudnn-7.2_bazel:V0.2 docker image will be created!"
+		docker build -t nv/cuda-9.2_cudnn-7.2_bazel:V0.2 .
+		echo "The nv/cuda-9.2_cudnn-7.2_bazel:V0.2 docker image has be created!"
+		if [ -f ubuntu-base-16.04.4-base-amd64.tar.gz ]; then
+			rm -f ubuntu-base-16.04.4-base-amd64.tar.gz
+		fi
+		if [ -f bazel-0.5.4-installer-linux-x86_64.sh ]; then
+			rm -f bazel-0.5.4-installer-linux-x86_64.sh
 		fi
 		popd
 		;;

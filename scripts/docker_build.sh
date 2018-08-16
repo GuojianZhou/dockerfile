7ec2fa08ad72c34754fe9831f5f65b8aa5dbf373
Joe <joe_zgj@163.com>
2018-08-16 17:36:36 +0800

    Add the CUDA and CUDNN dockerfile
    Signed-off-by: Joe <joe_zgj@163.com>

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
 
+DOCKER_IMAGE=$1
 TOP_DIR=`dirname ${PWD}`
-pushd ${TOP_DIR}/ubuntu/
-echo "The joe/ubuntu-16.04.4:V0.1 docker image will be created!"
-docker build -t joe/ubuntu-16.04.4:V0.1 .
-echo "The joe/ubuntu-16.04.4:V0.1 docker image has be created!"
-popd
+
+docker images
+docker ps -a
+echo "The docker image ${DOCKER_IMAGE} will been created on `date`"
+case ${DOCKER_IMAGE} in
+	"ubuntu")
+		pushd ${TOP_DIR}/ubuntu/
+		cp ${TOP_DIR}/images/ubuntu-base-16.04.4-base-amd64.tar.gz .
+		echo "The joe/ubuntu-16.04.4:V0.1 docker image will be created!"
+		docker build -t joe/ubuntu-16.04.4:V0.1 .
+		echo "The joe/ubuntu-16.04.4:V0.1 docker image has be created!"
+		if [ -f ubuntu-base-16.04.4-base-amd64.tar.gz ]; then
+			rm -f ubuntu-base-16.04.4-base-amd64.tar.gz
+		fi
+		popd
+		;;
+	"CUDA")
+		pushd ${TOP_DIR}/CUDA/CUDA-9.2.V0.1
+		cp ${TOP_DIR}/images/ubuntu-base-16.04.4-base-amd64.tar.gz .
+		echo "The nv/cuda-9.2_cudnn-7.2:V0.1 docker image will be created!"
+		docker build -t nv/cuda-9.2_cudnn-7.2:V0.1 .
+		echo "The nv/cuda-9.2_cudnn-7.2:V0.1:V0.1 docker image has be created!"
+		if [ -f ubuntu-base-16.04.4-base-amd64.tar.gz ]; then
+			rm -f ubuntu-base-16.04.4-base-amd64.tar.gz
+		fi
+		popd
+		;;
+	*)
+		pushd ${TOP_DIR}/ubuntu/
+		cp ${TOP_DIR}/images/ubuntu-base-16.04.4-base-amd64.tar.gz .
+		echo "The joe/ubuntu-16.04.4:V0.1 docker image will be created!"
+		docker build -t joe/ubuntu-16.04.4:V0.1 .
+		echo "The joe/ubuntu-16.04.4:V0.1 docker image has be created!"
+		if [ -f ubuntu-base-16.04.4-base-amd64.tar.gz ]; then
+			rm -f ubuntu-base-16.04.4-base-amd64.tar.gz
+		fi
+		popd
+		;;
+esac
+echo "The docker image ${DOCKER_IMAGE} has been created on `date`"
+docker images
+docker ps -a

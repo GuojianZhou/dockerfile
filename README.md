b089bc115c0404969dbef4e5149865b9adeed503
Joe <joe_zgj@163.com>
2018-08-24 14:20:45 +0800

    Add the new cuda-9.0 Dockerfile
    Signed-off-by: Joe <joe_zgj@163.com>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dockerfile
 build a ubuntu16.04 basic docker image
+
+If user want to create the bazel ubuntu image,
+need to download bazel-0.16.0-installer-linux-x86_64.sh
+tools to the images directory by manual.
--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -67,6 +67,25 @@ case ${DOCKER_IMAGE} in
 		fi
 		popd
 		;;
+	"CUDA-9.0-bazel-TF")
+		pushd ${TOP_DIR}/CUDA/CUDA-9.0.CUDNN-7.2
+		cp ${TOP_DIR}/images/ubuntu-base-16.04.5-base-amd64.tar.gz .
+		cp ${TOP_DIR}/images/bazel-0.16.0-installer-linux-x86_64.sh .
+		cp -a ${TOP_DIR}/images/py .
+		echo "The nv/cuda-9.0_cudnn-7.2_bazel_TF-1.10:V1.0 docker image will be created!"
+		docker build -t nv/cuda-9.0_cudnn-7.2_bazel_TF-1.10:V1.0 .
+		echo "The nv/cuda-9.0_cudnn-7.2_bazel_TF-1.10:V1.0 docker image has be created!"
+		if [ -f ubuntu-base-16.04.5-base-amd64.tar.gz ]; then
+			rm -f ubuntu-base-16.04.5-base-amd64.tar.gz
+		fi
+		if [ -f bazel-0.16.0-installer-linux-x86_64.sh ]; then
+			rm -f bazel-0.16.0-installer-linux-x86_64.sh
+		fi
+		if [ -d py ]; then
+			rm -fr py
+		fi
+		popd
+		;;
 	*)
 		pushd ${TOP_DIR}/ubuntu/
 		cp ${TOP_DIR}/images/ubuntu-base-16.04.5-base-amd64.tar.gz .

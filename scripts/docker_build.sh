7a52b8cf3b57acddab16c64e9a754c0ee0e75e09
Joe <joe_zgj@163.com>
2018-08-27 17:06:16 +0800

    Add the CUDA 8.0 and CUDNN 7.2  bazel-0.5.4 docker file to support TF-1.4
    Signed-off-by: Joe <joe_zgj@163.com>

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -86,6 +86,25 @@ case ${DOCKER_IMAGE} in
 		fi
 		popd
 		;;
+	"CUDA-8.0")
+		pushd ${TOP_DIR}/CUDA/CUDA-8.0.CUDNN-7.2
+		cp ${TOP_DIR}/images/ubuntu-base-16.04.5-base-amd64.tar.gz .
+		cp ${TOP_DIR}/images/bazel-0.5.4-installer-linux-x86_64.sh .
+		cp -a ${TOP_DIR}/images/py .
+		echo "The nv/cuda-8.0_cudnn-7.2_bazel_TF-1.4:V1.0 docker image will be created!"
+		docker build -t nv/cuda-9.0_cudnn-7.2_bazel_TF-1.10:V1.0 .
+		echo "The nv/cuda-8.0_cudnn-7.2_bazel_TF-1.4:V1.0 docker image has be created!"
+		if [ -f ubuntu-base-16.04.5-base-amd64.tar.gz ]; then
+			rm -f ubuntu-base-16.04.5-base-amd64.tar.gz
+		fi
+		if [ -f bazel-0.5.4-installer-linux-x86_64.sh ]; then
+			rm -f bazel-0.5.4-installer-linux-x86_64.sh
+		fi
+		if [ -d py ]; then
+			rm -fr py
+		fi
+		popd
+		;;
 	*)
 		pushd ${TOP_DIR}/ubuntu/
 		cp ${TOP_DIR}/images/ubuntu-base-16.04.5-base-amd64.tar.gz .
